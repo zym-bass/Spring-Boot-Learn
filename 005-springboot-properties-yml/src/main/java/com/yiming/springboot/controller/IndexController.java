8dcc48c6dd1388855bd04ea284a04ee91e80f25b
一名 <824122161@qq.com>
2021-02-05 09:52:09 +0800

    同时使用properties和yml,yaml配置
    默认使用properties文件

---
+++ 005-springboot-properties-yml/src/main/java/com/yiming/springboot/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.yiming.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/say")
+    public @ResponseBody
+    String say(){
+        return "hello springboot";
+    }
+}
