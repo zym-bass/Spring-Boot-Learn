e4d70615ed314bb601871c3657df4e8416f20053
一名 <824122161@qq.com>
2021-02-28 22:33:05 +0800

    springboot使用过滤器filter--注解

--- 031-springboot-servlet/src/main/java/com/yiming/springboot/config/ServletConfig.java
+++ 031-springboot-servlet/src/main/java/com/yiming/springboot/config/ServletConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration //该注解将此类定义为配置类（相当于一个xml配置文件）
-public class ServletConfig {  
+public class ServletConfig {
     //@Bean是一个方法级别上的注解，主要用在配置里
     //相当于一个<beans></beans>
     @Bean
---
+++ 032-springboot-filter-1/src/main/java/com/yiming/springboot/Application.java
@@ -0,0 +1,15 @@
+package com.yiming.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan(basePackages = "com.yiming.springboot.filter")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
